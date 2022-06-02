42b5fdb906a432f1740204f2478860888b0eaaf3
Alenkq <kunqi@usc.edu>
2022-06-01 17:12:00 -0700

    temp update6

--- static/js/loading2_ajax.js
+++ static/js/loading2_ajax.js
@@ -3,7 +3,15 @@ $(document).ready(function () {
         var aid_val = $(document.activeElement).val();
         $.get("/searching_details", {aid: aid_val}).done(function (data) {
             document.getElementById("loading-gif2").style.display = "none";
-            $('#aaaaaa').text(data).show();
+
+            var a_name = data.name;
+            var a_birthday = data.birthday;
+            var a_deadthday = data.deathday;
+            var a_nationality = data.nationality;
+            var a_biography = data.biography;
+
+            
+            // $('#aaaaaa').text(data).show();
         });
     
         event.preventDefault();
