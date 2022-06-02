d147ee3a3ec490080b4ef1486dc57e375930caf3
Alenkq <kunqi@usc.edu>
2022-06-01 21:29:27 -0700

    temp update6

--- static/js/a6sjs.js
+++ static/js/a6sjs.js
@@ -1,7 +1,10 @@
 function loading1(){
+    document.getElementById("details-area").style.display = "none";
     document.getElementById("loading-gif1").style.display = "block";
 }
 
 function loading2(){
+    document.getElementById("details-area").style.display = "block";
+    document.getElementById("details-box").style.display = "none";
     document.getElementById("loading-gif2").style.display = "block";
 }
--- static/js/loading2_ajax.js
+++ static/js/loading2_ajax.js
@@ -3,15 +3,28 @@ $(document).ready(function () {
         var aid_val = $(document.activeElement).val();
         $.get("/searching_details", {aid: aid_val}).done(function (data) {
             document.getElementById("loading-gif2").style.display = "none";
+            document.getElementById("details-box").style.display = 'block';
 
             var a_name = data.name;
             var a_birthday = data.birthday;
             var a_deadthday = data.deathday;
             var a_nationality = data.nationality;
             var a_biography = data.biography;
+            var htmlCode = '';
 
+            htmlCode = htmlCode + 
+
+                    `
+                    <div class="atitle">
+                        <p class="ap1">${a_name} (${a_birthday} - ${a_deadthday})</p>
+                        <p class="ap2">${a_nationality}</p>
+                    </div>
+                    <p><a id="a-bio"></a></p>
+                    `;
             
-            // $('#aaaaaa').text(data).show();
+            var details_box = document.querySelector('.details-box');
+            details_box.innerHTML = htmlCode;
+            $('#a-bio').text(a_biography).show();
         });
     
         event.preventDefault();
