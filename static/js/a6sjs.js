a2d6f0e16dd9678f0253d528394bc8f113f07fc9
Alenkq <kunqi@usc.edu>
2022-06-02 17:20:43 -0700

    update7

--- static/js/a6sjs.js
+++ static/js/a6sjs.js
@@ -35,35 +35,35 @@ function searching_bar_click(){
     });
 }
 
-function set_card_color(elem){
-    $(document).ready(function () {
-        var id = $(elem).attr("id");
-        var id = "#" + id;
-        var child_array = [];
+// function set_card_color(elem){
+//     $(document).ready(function () {
+//         var id = $(elem).attr("id");
+//         var id = "#" + id;
+//         var child_array = [];
 
-        $("#album > div").each((index, element) => {
-            // element == this
-            child_array.push(element.id);
-        });
+//         $("#album > div").each((index, element) => {
+//             // element == this
+//             child_array.push(element.id);
+//         });
 
-        for (var card_id of child_array) {
-            card_id = "#" + card_id + " >  button"
-            $(card_id).each((index, eleme) => {
-                // element == this
-                elem_id = '#' + eleme.id;
-                var button_color = $(elem_id).css("background-color");
+//         for (var card_id of child_array) {
+//             card_id = "#" + card_id + " >  button"
+//             $(card_id).each((index, eleme) => {
+//                 // element == this
+//                 elem_id = '#' + eleme.id;
+//                 var button_color = $(elem_id).css("background-color");
 
-                if (button_color == "rgb(17, 43, 60)") {
-                    $(elem_id).css("background-color", "#205375");
-                    // $(elem_id).mouseenter(function () { 
-                    //     $(this).css("background-color", "#112B3C");
-                    // }).mouseleave(function () {
-                    //     $(this).css("background-color", "#205375");
-                    // });
-                }
-            });
-        };
+//                 if (button_color == "rgb(17, 43, 60)") {
+//                     $(elem_id).css("background-color", "#205375");
+//                     // $(elem_id).mouseenter(function () { 
+//                     //     $(this).css("background-color", "#112B3C");
+//                     // }).mouseleave(function () {
+//                     //     $(this).css("background-color", "#205375");
+//                     // });
+//                 }
+//             });
+//         };
 
-        $(id).css("background-color", "#112B3C");
-    });
-}
+//         $(id).css("background-color", "#112B3C");
+//     });
+// }
--- static/js/loading1_ajax.js
+++ static/js/loading1_ajax.js
@@ -33,6 +33,12 @@ $(document).ready(function () {
                 var htmlCode = '';
                 var count = 0;
 
+                var album_origin = document.getElementById('album');
+                
+                while (album_origin.firstChild) {
+                    album_origin.removeChild(album_origin.firstChild);
+                };
+
                 for (const element of results_filter) {
                     var img_link = '';
                     var aid = element[1].substring(element[1].lastIndexOf("/") + 1, element[1].length);
@@ -45,26 +51,85 @@ $(document).ready(function () {
                         img_link = img_link + element[2];
                     }
 
-                    htmlCode = htmlCode + 
+                    // htmlCode = htmlCode + 
 
-                    `
-                    <div class="card" id="${count_c}">
-                        <button type="submit" name="aid" id="${count_s}" value="${aid}" onclick="set_card_color(this);">
-                            <div class="button-box">
-                                <img src="${img_link}">
-                                <div class="card-title">
-                                    ${element[0]}
-                                </div>
-                            </div>
-                        </button>
-                    </div>`;
+                    // `
+                    // <div class="card" id="${count_c}">
+                    //     <button type="submit" name="aid" id="${count_s}" value="${aid}" onclick="set_card_color(this);">
+                    //         <div class="button-box">
+                    //             <img src="${img_link}">
+                    //             <div class="card-title">
+                    //                 ${element[0]}
+                    //             </div>
+                    //         </div>
+                    //     </button>
+                    // </div>`;
+
+                    var card_div = document.createElement("div");
+                    card_div.className = "card";
+                    card_div.setAttribute("id", count_c);
+
+                    var card_button = document.createElement("button");
+                    card_button.type = "submit";
+                    card_button.name = "aid";
+                    card_button.id = count_s;
+                    card_button.value = aid;
+                    card_button.onclick = function (){
+                        var id = $(this).attr("id");
+                        var id = "#" + id;
+                        var child_array = [];
+                
+                        $("#album > div").each((index, element) => {
+                            // element == this
+                            child_array.push(element.id);
+                        });
+                
+                        for (var card_id of child_array) {
+                            card_id = "#" + card_id + " >  button"
+                            $(card_id).each((index, eleme) => {
+                                // element == this
+                                elem_id = '#' + eleme.id;
+                                var button_color = $(elem_id).css("background-color");
+                
+                                if (button_color == "rgb(17, 43, 60)") {
+                                    $(elem_id).css("background-color", "#205375");
+                                    // $(elem_id).mouseenter(function () { 
+                                    //     $(this).css("background-color", "#112B3C");
+                                    // }).mouseleave(function () {
+                                    //     $(this).css("background-color", "#205375");
+                                    // });
+                                }
+                            });
+                        };
+                
+                        $(id).css("background-color", "#112B3C");
+                    }
+
+                    var button_box = document.createElement("div");
+                    button_box.className = "button-box";
+
+                    var card_img = document.createElement("img");
+                    card_img.src = img_link;
+
+                    var card_title = document.createElement("div");
+                    card_title.className = "card-title";
+                    var title_node = document.createTextNode(element[0]);
+
+                    card_title.appendChild(title_node);
+                    button_box.appendChild(card_img);
+                    button_box.appendChild(card_title);
+                    card_button.appendChild(button_box);
+                    card_div.appendChild(card_button);
+
+                    var album = document.getElementById('album');
+                    album.appendChild(card_div);
 
                     count = count + 1;
 
                 }
                 
-                var album = document.querySelector('.album');
-                album.innerHTML = htmlCode;
+                // var album = document.querySelector('.album');
+                // album.innerHTML = htmlCode;
                 // $('#aaaaaa').text(results_filter).show();
             }
          });
